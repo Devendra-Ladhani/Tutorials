be07864c8ef8fb9953b5d2e90b020c79ed6ef695
Devendra-Ladhani <devladhani28@gmail.com>
2025-04-13 12:39:12 +0530

    [ADD]07reactRouter: Completed till 12th video

---
+++ ReactTutorial/ChaiAurReact/07reactRouter/src/components/index.js
@@ -0,0 +1,10 @@
+import Home from './Home/home'
+import About from './About/About'
+import Footer from './Footer/footer'
+import Github from './Github/Github'
+import Header from './Header/header'
+import Contact from './Contact/Contact'
+import User from './user/User'
+
+
+export {About, Home, Footer, Github,Header, Contact, User}
